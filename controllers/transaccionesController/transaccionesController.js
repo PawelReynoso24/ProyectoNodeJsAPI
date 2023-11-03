eae5c623e0cd8697966287f8c60acc0c1308c1fc
PawelReynoso24 <99044276+PawelReynoso24@users.noreply.github.com>
2023-11-03 13:00:58 -0600

    API de tanque lista

--- controllers/transaccionesController/transaccionesController.js
+++ controllers/transaccionesController/transaccionesController.js
@@ -36,27 +36,80 @@ module.exports = {
 
 
     //create
-   async create (req, res) {
-        let datos = req.body //Serializar los datos
-        const datos_ingreso = { //Objeto
-            idEstacion: datos.idEstacion,
-            cantidadVendida: datos.cantidadVendida,
-            precioGalon: datos.precioGalon,
-            idCliente: datos.idCliente,
-            fecha: datos.fecha,
-            tipoPago: datos.tipoPago,
-            descuento: datos.descuento,
-            empleado: datos.empleado
-        };
+//    async create (req, res) {
+//     try {
+//             const datos = req.body //Serializar los datos
+//             const datos_ingreso = { //Objeto
+//                 idEstacion: datos.idEstacion,
+//                 cantidadVendida: datos.cantidadVendida,
+//                 precioGalon: datos.precioGalon,
+//                 idCliente: datos.idCliente,
+//                 fecha: datos.fecha,
+//                 tipoPago: datos.tipoPago,
+//                 descuento: datos.descuento,
+//                 empleado: datos.empleado
+//             };
 
-        TRANSACCION.create(datos_ingreso)
-        .then(transacciones => {
-            res.send(transacciones);
-        })
-        .catch(error => {
-            console.log(error)
-            return res.status(500).json({ error: 'Error al insertar' });
-        });
-    },
+//                 // TRANSACCION.create(datos_ingreso)
+//                 // .then(transacciones => {
+//                 //     res.send(transacciones);
+//                 // })
+//                 // .catch(error => {
+//                 //     console.log(error)
+//                 //     return res.status(500).json({ error: 'Error al insertar' });
+//                 // });
+//         }
+//     },
+
+async create(req, res) {
+    try {
+        const datos = req.body; // Serializar los datos
+
+       // const transacciones = await TRANSACCION.create(datos_ingreso);
+
+        const nivelActualResponse = await axios.get(`http://localhost:8080/apiSPTanques/obtenerNivelTanque/${datos.idEstacion}`);
+        const nivelActual = nivelActualResponse.data;
+
+        const nuevoNivel = nivelActual - datos.cantidadVendida;
+
+        const options = {
+
+            method: 'PUT',
+            url: 'http://localhost:8080/apiSPTanques/actualizarNivelTanque',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+
+            data: {
+                
+                request:{
+                    nuevo_nivel: nuevoNivel,
+                    tanques:{
+                        id: datos.idEstacion,
+                        nivel_actual: nuevoNivel
+                    }
+                }
+            
+            }
+        }
+
+        try {
+            const result = await axios(options);
+            
+            if (result && result.data) {
+                // Si la respuesta contiene datos, envía esos datos en la respuesta de tu API
+                res.status(200).json(result.data);
+            } else {
+                res.status(404).send("No se encontraron datos de tanques");
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error al obtener los datos de los tanques de gasolina");
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Error al insertar o actualizar el nivel del tanque' });
+    }
+}
    
 };
